f168f2e710c4c9d496137067b77b6ac11c4ee2d8
kwonsm <kwon-s-m@hanmail.net>
2015-10-13 09:20:30 +0900

    N/A

--- exof/src/main/java/balam/exof/App.java
+++ exof/src/main/java/balam/exof/App.java
@@ -1,13 +1,14 @@
 package balam.exof;
 
 /**
- * Hello world!
+ * 
+ * @author kwonsm
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        
     }
 }
