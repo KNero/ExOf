d3c540dccc09ef9c8bc4c3fad40cd41965db903e
kwonsm <kwon-s-m@hanmail.net>
2015-10-20 17:44:15 +0900

    operator추가

--- exof/src/main/java/balam/exof/App.java
+++ exof/src/main/java/balam/exof/App.java
@@ -10,16 +10,29 @@ import balam.exof.environment.MainLoader;
  */
 public class App 
 {
+	public static void start()
+	{
+		 try
+	        {
+	        	Loader mainLoader = new MainLoader();
+	        	mainLoader.load();
+	        }
+	        catch(Exception e)
+	        {
+	        	e.printStackTrace();
+	        }
+	        
+	        Operator.init();
+	        Operator.start();
+	}
+	
+	public static void stop()
+	{
+		Operator.stop();
+	}
+	
     public static void main(String[] args)
     {
-        try
-        {
-        	Loader mainLoader = new MainLoader();
-        	mainLoader.load();
-        }
-        catch(Exception e)
-        {
-        	e.printStackTrace();
-        }
+       start();
     }
 }
