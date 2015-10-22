37cbcce661feb5b244839672910aae25a2e21e6b
kwonsm <kwon-s-m@hanmail.net>
2015-10-22 17:33:46 +0900

    메시지 추가

--- exof/src/main/java/balam/exof/scheduler/ReloadException.java
+++ exof/src/main/java/balam/exof/scheduler/ReloadException.java
@@ -4,4 +4,13 @@ public class ReloadException extends Exception
 {
 	private static final long serialVersionUID = 166292007538067111L;
 
+	public ReloadException(String _key, String _value)
+	{
+		super("Parameter reloading failed. [" + _key + "] = [" + _value + "]");
+	}
+	
+	public ReloadException(String _msg)
+	{
+		super("Parameter reloading failed. (" + _msg + ")");
+	}
 }
