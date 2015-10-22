040bb29f5e419127fe8c9d5387659f007644f81c
kwonsm <kwon-s-m@hanmail.net>
2015-10-22 17:34:08 +0900

    생성자 추가

--- exof/src/main/java/balam/exof/scheduler/SchedulerExecuteException.java
+++ exof/src/main/java/balam/exof/scheduler/SchedulerExecuteException.java
@@ -4,4 +4,8 @@ public class SchedulerExecuteException extends Exception
 {
 	private static final long serialVersionUID = 7745806199049680332L;
 
+	public SchedulerExecuteException(String _msg)
+	{
+		super(_msg);
+	}
 }
