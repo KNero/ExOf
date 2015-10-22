983b91907104ad24f870b04c78354fcacab81890
kwonsm <kwon-s-m@hanmail.net>
2015-10-22 17:37:05 +0900

    스케쥴러 설정 및 파라미터 설정 전달 수정

--- exof/src/main/java/balam/exof/scheduler/SchedulerJob.java
+++ exof/src/main/java/balam/exof/scheduler/SchedulerJob.java
@@ -1,13 +1,9 @@
 package balam.exof.scheduler;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Observable;
-import java.util.Observer;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.quartz.Job;
+import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
@@ -15,54 +11,29 @@ import org.slf4j.LoggerFactory;
 
 import balam.exof.environment.EnvKey;
 import balam.exof.util.CircularList;
-import balam.exof.util.CollectionUtil;
 
-public abstract class SchedulerJob implements Job, Observer
+public abstract class SchedulerJob implements Job
 {
 	protected Logger logger = LoggerFactory.getLogger(this.getClass());
 	
-	private static String name;
-	private static Boolean isDuplicateExecution;
-	private static AtomicBoolean isExecute = new AtomicBoolean(false);
-	protected static CircularList<Parameter> paramList;
+	public static final String IS_RUNNING = "isRunning";
 	
 	abstract void execute(Parameter _param) throws SchedulerExecuteException;
 	
-	private void _init(JobExecutionContext _arg)
-	{
-		if(name == null)
-		{
-			name = (String)_arg.getMergedJobDataMap().remove("name");
-			isDuplicateExecution = (Boolean)_arg.getMergedJobDataMap().remove(EnvKey.Service.DUPLICATE);
-			
-			@SuppressWarnings("unchecked")
-			List<Map<String, ?>> paramGroup = (List<Map<String, ?>>)_arg.getMergedJobDataMap().remove(EnvKey.Service.PARAM_GROUP);
-			if(paramGroup != null)
-			{
-				List<Parameter> list = new LinkedList<Parameter>();
-				CollectionUtil.doIterator(paramGroup, _param -> {
-					Parameter p = new Parameter();
-					list.add(p);
-					
-					CollectionUtil.doIterator(_param.keySet(), _key -> {
-						p.set(_key, _param.get(_key));
-					});
-				});
-				
-				paramList = new CircularList<Parameter>(list);
-			}
-		}
-	}
-
 	@Override
 	final public void execute(JobExecutionContext _arg) throws JobExecutionException 
 	{
-		this._init(_arg);
+		JobDataMap jobData = _arg.getMergedJobDataMap();
 		
-		if(! (isDuplicateExecution || isExecute.compareAndSet(false, true))) return;
+		AtomicBoolean isRunning = (AtomicBoolean)jobData.get(SchedulerJob.IS_RUNNING);
+		Boolean isDuplicateExecution = (Boolean)jobData.get(EnvKey.Service.DUPLICATE);
+		
+		if(! (isDuplicateExecution || isRunning.compareAndSet(false, true))) return;
+		
+		@SuppressWarnings("unchecked")
+		CircularList<Parameter> paramGroup = (CircularList<Parameter>)jobData.get(EnvKey.Service.PARAM_GROUP);
+		Parameter param = paramGroup.next();
 
-		Parameter param = paramList.next();
-		
 		try
 		{
 			this.execute(param);
@@ -73,13 +44,7 @@ public abstract class SchedulerJob implements Job, Observer
 		}
 		finally
 		{
-			isExecute.set(false);
+			isRunning.set(false);
 		}
-	}
-	
-	@Override
-	final public void update(Observable _o, Object _arg)
-	{
-		
 	}
 }
