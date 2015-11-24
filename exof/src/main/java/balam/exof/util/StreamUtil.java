00456b122d233d6f0008a67e9b3a8a930e50fc69
kwonsm <kwon-s-m@hanmail.net>
2015-11-24 09:58:40 +0900

    주석 추가

--- exof/src/main/java/balam/exof/util/StreamUtil.java
+++ exof/src/main/java/balam/exof/util/StreamUtil.java
@@ -6,6 +6,13 @@ import java.io.InputStream;
 
 public class StreamUtil
 {
+	/**
+	 * InputStream 중 원하는 길이만큼 데이터를 읽는다.
+	 * @param _in 대상 InputStream
+	 * @param _length 읽어야할 데이터 길이
+	 * @return 읽어들인 데이터 길이
+	 * @throws IOException
+	 */
 	public static byte[] read(InputStream _in, int _length) throws IOException
 	{
 		ByteArrayOutputStream resultBuf = new ByteArrayOutputStream(_length);
