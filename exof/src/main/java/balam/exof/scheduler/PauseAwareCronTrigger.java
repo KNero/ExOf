3db9d3c8d6e7ac78ed0ef7ee17f09d1ef8cc4207
kwonsm <kwon-s-m@hanmail.net>
2016-02-04 10:55:44 +0900

    class의 사용이유 주석추가

--- exof/src/main/java/balam/exof/scheduler/PauseAwareCronTrigger.java
+++ exof/src/main/java/balam/exof/scheduler/PauseAwareCronTrigger.java
@@ -6,6 +6,13 @@ import java.util.UUID;
 import org.quartz.CronExpression;
 import org.quartz.impl.triggers.CronTriggerImpl;
 
+/**
+ * 스케쥴러의 실행 시간이 지연될 경우 다음 스케쥴이 
+ * 호출될 때 그전에 호출되지 못한 모든 트리거를 실행한다.
+ * 그 부분을 방지하기 위해서 nextFireTime을 업데이트 해준다.
+ * @author kwonsm
+ *
+ */
 public class PauseAwareCronTrigger extends CronTriggerImpl
 {
 	private static final long serialVersionUID = -2609414523458705574L;
