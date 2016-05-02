790731126b2e43be2d807c3a7f82209370b1d805
kwonsm <kwon-s-m@hanmail.net>
2016-05-02 13:17:30 +0900

    Setting interface 삭제

--- exof/src/main/java/team/balam/exof/environment/SystemSetting.java
+++ exof/src/main/java/team/balam/exof/environment/SystemSetting.java
@@ -9,7 +9,7 @@ import java.util.Map;
  * @author kwonsm
  *
  */
-public class SystemSetting implements Setting
+public class SystemSetting
 {
 	private Map<String, Object> setting = new HashMap<String, Object>();
 	
@@ -17,63 +17,54 @@ public class SystemSetting implements Setting
 	
 	private SystemSetting() {}
 	
-	public static Setting getInstance()
+	public static SystemSetting getInstance()
 	{
 		return self;
 	}
 	
-	@Override
 	public void set(String _prefix, String _key, Object _value) 
 	{
 		this.setting.put(_prefix + _key, _value);
 	}
 	
-	@Override
 	public Object get(String _prefix, String _key)
 	{
 		return this.setting.get(_prefix + _key);
 	}
 	
-	@Override
 	public Object getAndRemove(String _prefix, String _key)
 	{
 		return this.setting.remove(_prefix + _key);
 	}
 
-	@Override
 	public String getString(String _prefix, String _key) 
 	{
 		return this.setting.get(_prefix + _key).toString();
 	}
 
-	@Override
 	public String getStringAndRemove(String _prefix, String _key) 
 	{
 		return this.setting.remove(_prefix + _key).toString();
 	}
 
-	@Override
 	@SuppressWarnings("unchecked")
 	public <T> List<T> getList(String _prefix, String _key) 
 	{
 		return (List<T>)this.setting.get(_prefix + _key);
 	}
 
-	@Override
 	@SuppressWarnings("unchecked")
 	public <T> List<T> getListAndRemove(String _prefix, String _key) 
 	{
 		return (List<T>)this.setting.remove(_prefix + _key);
 	}
 
-	@Override
 	@SuppressWarnings("unchecked")
 	public <K, V> Map<K, V> getMap(String _prefix, String _key) 
 	{
 		return (Map<K, V>)this.setting.get(_prefix + _key);
 	}
 
-	@Override
 	@SuppressWarnings("unchecked")
 	public <K, V> Map<K, V> getMapAndRemove(String _prefix, String _key) 
 	{
