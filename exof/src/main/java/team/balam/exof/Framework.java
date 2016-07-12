5d255046d1eaf2b5edf032f4aa9e0f0e16687f45
kwonsm <kwon-s-m@hanmail.net>
2016-07-12 09:28:06 +0900

    jetty module 추가

--- exof/src/main/java/team/balam/exof/Framework.java
+++ exof/src/main/java/team/balam/exof/Framework.java
@@ -3,12 +3,13 @@ package team.balam.exof;
 import team.balam.exof.environment.FileModifyChecker;
 import team.balam.exof.listener.Listener;
 import team.balam.exof.service.ServiceProvider;
+import team.balam.exof.was.JettyModule;
 
 
 
 public class Framework implements Container
 {
-	private Module[] moduleList = new Module[]{Listener.getInstance(), ServiceProvider.getInstance()};
+	private Module[] moduleList = new Module[]{Listener.getInstance(), ServiceProvider.getInstance(), new JettyModule()};
 	private FileModifyChecker fileModifyChecker;
 	
 	@Override
