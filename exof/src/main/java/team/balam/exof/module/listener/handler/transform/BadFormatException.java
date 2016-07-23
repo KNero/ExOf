7549345d32a877f35f2d1ead6f213aaf7162f0ba
kwonsm <kwonsm@172.20.10.3>
2016-07-23 22:20:37 +0900

    BadFormatExceptio을 상위로 던질 경우 클라이언트 세션이 닫긴다.

---
+++ exof/src/main/java/team/balam/exof/module/listener/handler/transform/BadFormatException.java
@@ -0,0 +1,11 @@
+package team.balam.exof.module.listener.handler.transform;
+
+public class BadFormatException extends Exception
+{
+	private static final long serialVersionUID = 7167793189718703667L;
+	
+	public BadFormatException(String _msg)
+	{ 
+		super(_msg);
+	}
+}
