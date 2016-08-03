bd962688b72ade1da5c47e7ea9ada78ec9fb461e
kwonsm <kwonsm@172.20.10.3>
2016-08-03 14:39:08 +0900

    json 값 중 service path를 가져올 수 있는 key값 설정 추가

--- exof/src/main/java/team/balam/exof/module/listener/handler/transform/JsonTransform.java
+++ exof/src/main/java/team/balam/exof/module/listener/handler/transform/JsonTransform.java
@@ -9,34 +9,30 @@ import team.balam.exof.module.service.ServiceObject;
 
 public class JsonTransform implements ServiceObjectTransform<String>
 {
-	private ObjectMapper jsonMapper = new ObjectMapper();
-	private TypeReference<HashMap<String, Object>> mapType = new TypeReference<HashMap<String, Object>>(){};
+	protected ObjectMapper jsonMapper = new ObjectMapper();
+	protected TypeReference<HashMap<String, Object>> mapType = new TypeReference<HashMap<String, Object>>(){};
 	
+	protected String servicePathKey = "servicePath";
+	
+	public String getServicePathKey()
+	{
+		return servicePathKey;
+	}
+
+	public void setServicePathKey(String servicePathKey)
+	{
+		this.servicePathKey = servicePathKey;
+	}
+
 	@Override
 	public ServiceObject transform(String _msg) throws Exception 
 	{
 		HashMap<String, Object> requestMap = this.jsonMapper.readValue(_msg, this.mapType);
 		
 		//타입키 관련 클래스추가
-		ServiceObject serviceObject = new ServiceObject((String)requestMap.remove(""));
+		ServiceObject serviceObject = new ServiceObject((String)requestMap.get(this.servicePathKey));
 		serviceObject.setRequest(requestMap);
 		
 		return serviceObject;
 	}
-	
-//	public static void main(String[] _a) throws Exception
-//	{
-//		HashMap<String, Object> map = new HashMap<String, Object>();
-//		map.put(TagKey.SERVICE_PATH, "/test/test");
-//		map.put("name", "권성민");
-//		map.put("test", 123);
-//		
-//		ObjectMapper mapper = new ObjectMapper();
-//		String value = mapper.writeValueAsString(map);
-//		System.out.println(value);
-//		
-//		TypeReference<HashMap<String, Object>> type = new TypeReference<HashMap<String, Object>>(){};
-//		HashMap<String, Object> result = mapper.readValue(value, type);
-//		System.out.println(result);
-//	}
 }
