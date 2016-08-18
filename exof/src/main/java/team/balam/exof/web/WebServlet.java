11756b12e99eb3c16fd20d8508cffb0759100c04
kwonsm <kwonsm@172.20.10.3>
2016-08-18 16:55:46 +0900

    HttpServletTransform이 null을 리턴할 경우 UnavailableException 발생

--- exof/src/main/java/team/balam/exof/web/WebServlet.java
+++ exof/src/main/java/team/balam/exof/web/WebServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.UnavailableException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -102,6 +103,13 @@ public class WebServlet extends HttpServlet
 				this.logger.error("Can not transform http message.", e);
 				
 				throw new ServletException(e);
+			}
+			
+			if(serviceObject == null)
+			{
+				this.logger.error("ServiceObject is null");
+				
+				throw new UnavailableException(req.getMethod() + " is not available");
 			}
 		}
 		else
