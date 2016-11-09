fd915730b06bc42220972d529457e001a0f7baf1
kwonsm <kwon-s-m@hanmail.net>
2016-11-09 13:43:54 +0900

    commit

---
+++ exof/src/main/java/team/balam/exof/util/JarFileLoader.java
@@ -0,0 +1,58 @@
+package team.balam.exof.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class JarFileLoader 
+{
+	@SuppressWarnings( "rawtypes" )
+	private static final Class[] parameters = new Class[] { URL.class };
+	
+	public static void load( String _path ) throws Exception
+	{
+		File f = new File( _path );
+		
+		if( f.isDirectory() )
+		{
+			for( String fileName : f.list() )
+			{
+				JarFileLoader.load( _path + "/" + fileName );
+			}
+		}
+		else if( f.isFile() && f.getName().endsWith( ".jar" ) )
+		{
+			JarFileLoader.addFile( f );
+		}
+	}
+	
+	public static void addFile( String _s ) throws Exception
+	{
+		File f = new File( _s );
+		JarFileLoader.addFile( f );
+	}
+	
+	public static void addFile( File _f ) throws IOException
+	{
+		JarFileLoader.addURL( _f.toURI( ).toURL( ) );
+	}
+	
+	public static void addURL( URL _u ) throws IOException
+	{
+		URLClassLoader sysloader = ( URLClassLoader ) ClassLoader.getSystemClassLoader( );
+		Class< URLClassLoader > sysclass = URLClassLoader.class;
+		
+		try
+		{
+			Method method = sysclass.getDeclaredMethod( "addURL" , parameters );
+			method.setAccessible( true );
+			method.invoke( sysloader , new Object[] { _u } );
+		}
+		catch( Exception e )
+		{
+			throw new IOException( "Error, could not add URL to system classloader" );
+		}
+	}
+}
