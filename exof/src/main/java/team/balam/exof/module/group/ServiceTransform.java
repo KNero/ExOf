c7616a82f93bd7c0da87168bdebcfc38596aa6e5
kwonsm <kwonsm@218.38.137.8>
2017-02-20 14:46:35 +0900

    group listener 구조 추가.

---
+++ exof/src/main/java/team/balam/exof/module/group/ServiceTransform.java
@@ -0,0 +1,13 @@
+package team.balam.exof.module.group;
+
+import team.balam.exof.module.listener.handler.transform.ServiceObjectTransform;
+import team.balam.exof.module.service.ServiceObject;
+
+public class ServiceTransform implements ServiceObjectTransform<byte[]>
+{
+	@Override
+	public ServiceObject transform(byte[] _msg) throws Exception
+	{
+		return null;
+	}
+}
