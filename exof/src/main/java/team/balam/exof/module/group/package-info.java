6095625cbf0dfbb834d2754271d4e26dad5f6e24
kwonsm <kwonsm@218.38.137.8>
2017-02-20 15:41:40 +0900

    group 기능 삭제.

--- exof/src/main/java/team/balam/exof/module/group/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * ExOf 노드를 Socket으로 연결하여 group을 만드는 기능을 지원하는 패키지.<br>
- * 각 Session은 비동기 통신을 하며 응답에 상관없이 요청을 보내며 받은 응답은 map에 저장하게 된다.
- */
-/**
- * @author kwonsm
- *
- */
-package team.balam.exof.module.group;
