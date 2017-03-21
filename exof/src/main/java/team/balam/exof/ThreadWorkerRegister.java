32083411c11b733e5b08216f98675f1d788e8f77
kwonsm <kwonsm@icloud.com>
2017-03-21 14:34:28 +0900

    ThreadWorker 추가 시 null 검사와 에러로그 추가

--- exof/src/main/java/team/balam/exof/ThreadWorkerRegister.java
+++ exof/src/main/java/team/balam/exof/ThreadWorkerRegister.java
@@ -3,6 +3,9 @@ package team.balam.exof;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * 여기에 등록된 Thread는 정기적으로 죽었는지 체크하여 ThreadWorker의 createIfStop()을 호출해 준다.
  * @author kwonsm
@@ -10,6 +13,8 @@ import java.util.concurrent.CopyOnWriteArrayList;
  */
 public class ThreadWorkerRegister
 {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	private List<ThreadWorker> workerList;
 	private static final ThreadWorkerRegister self = new ThreadWorkerRegister();
 	
@@ -25,9 +30,18 @@ public class ThreadWorkerRegister
 	
 	public void add(ThreadWorker _worker)
 	{
+		if(_worker == null)
+		{
+			this.logger.error("ThreadWorker is null.", new NullPointerException());
+		}
+		
 		if(! this.workerList.contains(_worker))
 		{
 			this.workerList.add(_worker);
+		}
+		else
+		{
+			this.logger.error("ThreadWorker is already exists. => " + _worker.getName());
 		}
 	}
 	
@@ -42,6 +56,8 @@ public class ThreadWorkerRegister
 		{
 			if(worker.isStop() || ! worker.isAlive())
 			{
+				this.logger.error("ThreadWorker[" + worker.getName() +"] is stoppted. Created ThreadWorker.");
+				
 				this.workerList.remove(worker);
 				this.workerList.add(worker.createIfStop());
 			}
