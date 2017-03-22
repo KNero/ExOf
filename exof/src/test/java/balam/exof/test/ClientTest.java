770b8baa4d40aa84f94956bedac0a216475247e7
kwonsm <kwonsm@localhost>
2017-03-22 14:28:45 +0900

    maven build를 위한 TestCase 정리.

--- exof/src/test/java/balam/exof/test/ClientTest.java
+++ exof/src/test/java/balam/exof/test/ClientTest.java
@@ -10,48 +10,55 @@ import io.netty.handler.codec.DelimiterBasedFrameDecoder;
 import io.netty.handler.codec.Delimiters;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
+import junit.framework.Assert;
 import team.balam.exof.Constant;
 import team.balam.exof.client.Sender;
-import team.balam.exof.container.console.client.InfoGetter;
+import team.balam.exof.container.console.CommandBuilder;
+import team.balam.exof.container.console.client.Client;
 import team.balam.exof.module.listener.handler.ChannelHandlerMaker;
 
 public class ClientTest
 {
 	@Test
-	public void getScheduleList()
+	public void testSender() throws Exception
 	{
-		InfoGetter getter = new InfoGetter();
-		getter.getScheduleList();
+		Sender<String, String> client = new Sender<>(new ChannelHandlerMaker(){
+			@Override
+			public ChannelHandler[] make(SocketChannel _socketChannel) 
+			{
+				return new ChannelHandler[]{new StringEncoder(), 
+						new DelimiterBasedFrameDecoder(2048, Delimiters.nulDelimiter()),
+						new StringDecoder(Charset.forName(Constant.NETWORK_CHARSET))};
+			}
+		});
+		
+		client.setConnectTimeout(5000);
+		client.setReadTimeout(3000);
+		client.connect("localhost", 2000);
+
+		String res = client.sendAndWait("{\"aaa\":\"ababa\", \"servicePath\":\"/test/receive\"}\0");
+		client.close();
+		
+		Assert.assertEquals(res, "response");
 	}
 	
 	@Test
-	public void send() throws Exception
+	public void testConsoleGetServiceList() throws Exception
 	{
-		try
-		{
-			Sender<String, String> client = new Sender<>(new ChannelHandlerMaker(){
-				@Override
-				public ChannelHandler[] make(SocketChannel _socketChannel) 
-				{
-					return new ChannelHandler[]{new StringEncoder(), 
-							new DelimiterBasedFrameDecoder(2048, Delimiters.nulDelimiter()),
-							new StringDecoder(Charset.forName(Constant.NETWORK_CHARSET))};
-				}
-			});
+		Client.init();
+		
+		Client.send(CommandBuilder.buildServiceListGetter(), _result -> {
 			
-			client.setConnectTimeout(5000);
-			client.setReadTimeout(3000);
-			client.connect("127.0.0.1", 2000);
-
-			String res = client.sendAndWait("{\"aaa\":\"ababa\", \"servicePath\":\"/test/receive\"}\0");
-			System.out.println("============>" + res);
+		});
+	}
+	
+	@Test
+	public void testConsoleGetScheduleList() throws Exception
+	{
+		Client.init();
+		
+		Client.send(CommandBuilder.buildScheduleListGetter(), _result -> {
 			
-			client.close();
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-			throw e;
-		}
+		});
 	}
 }
---
+++ exof/src/test/java/balam/exof/test/ThreadWorkerTest.java
@@ -0,0 +1,73 @@
+package balam.exof.test;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+import team.balam.exof.ThreadWorker;
+import team.balam.exof.ThreadWorkerRegister;
+
+public class ThreadWorkerTest extends ThreadWorker
+{
+	private static boolean isCreateNewWorker;
+	private int count;
+	
+	@Override
+	public void run()
+	{
+		while(true)
+		{
+			++this.count;
+			
+			try
+			{
+				Thread.sleep(1000);
+			}
+			catch(InterruptedException e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	@Override
+	public boolean isStop()
+	{
+		return this.count % 5 == 0;
+	}
+
+	@Override
+	public ThreadWorker createIfStop()
+	{
+		ThreadWorkerTest.isCreateNewWorker = true;
+		
+		System.out.println("create new threadworker.");
+		ThreadWorker tw = new ThreadWorkerTest();
+		tw.start();
+		return tw;
+	}
+	
+	@Test
+	public void testThreadWorker()
+	{
+		ThreadWorker tw = new ThreadWorkerTest();
+		ThreadWorkerRegister.getInstance().add(tw);
+		
+		tw.start();
+		
+		for(int i = 0; i < 10; ++i)
+		{
+			ThreadWorkerRegister.getInstance().check();
+			
+			try
+			{
+				Thread.sleep(1000);
+			}
+			catch(InterruptedException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		
+		Assert.assertEquals(true, ThreadWorkerTest.isCreateNewWorker);
+	}
+}
