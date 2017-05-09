bf68805d293200402dca721d0fb0aceb985e7c68
KwonSeongmin <kwonsm@icloud.com>
2017-05-09 22:19:31 +0900

    loginWebConsole service 추가.

--- exof/src/main/java/team/balam/exof/container/console/ConsoleService.java
+++ exof/src/main/java/team/balam/exof/container/console/ConsoleService.java
@@ -5,35 +5,48 @@ import java.util.List;
 import java.util.Map;
 
 import team.balam.exof.container.SchedulerManager;
+import team.balam.exof.environment.EnvKey;
+import team.balam.exof.environment.SystemSetting;
+import team.balam.exof.module.listener.PortInfo;
 import team.balam.exof.module.service.ServiceProvider;
 
-public class ConsoleService
-{
-	public Object getServiceList(Map<String, Object> _param)
-	{
-		Map<String, HashMap<String, Object>> result = ServiceProvider.getInstance().getAllServiceInfo();
-		if(result.size() == 0)
-		{
-			return Command.NO_DATA_RESPONSE;
+public class ConsoleService {
+	public Object loginWebConsole(Map<String, Object> _param) {
+		String id = (String)_param.get("id");
+		String password = (String)_param.get("password");
+
+		PortInfo port = (PortInfo)SystemSetting.getInstance().get(EnvKey.FileName.LISTENER, EnvKey.Listener.WEB_CONSOLE);
+		String portId = port.getAttribute(EnvKey.Listener.ID);
+		String portPw = port.getAttribute(EnvKey.Listener.PASSWORD);
+		
+		if (portId != null && portId.equals(id)) {
+			if (portPw != null && portPw.equals(password)) {
+				return WebServlet.LOGIN_SUCCESS;
+			}
 		}
-		else
-		{
+		
+		return Command.NO_DATA_RESPONSE;
+	}
+	
+	public Object getServiceList(Map<String, Object> _param) {
+		Map<String, HashMap<String, Object>> result = ServiceProvider.getInstance().getAllServiceInfo();
+		
+		if (result.size() == 0) {
+			return Command.NO_DATA_RESPONSE;
+		} else {
 			return result;
 		}
 	}
-	
-	public Object getScheduleList(Map<String, Object> _param)
-	{
+
+	public Object getScheduleList(Map<String, Object> _param) {
 		List<String> list = SchedulerManager.getInstance().getScheduleList();
-		if(list.size() == 0)
-		{
+		
+		if (list.size() == 0) {
 			return Command.NO_DATA_RESPONSE;
-		}
-		else
-		{
+		} else {
 			Map<String, Object> result = new HashMap<>();
 			result.put("list", list);
-			
+
 			return result;
 		}
 	}
