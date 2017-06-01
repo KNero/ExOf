c86869e1bd9e7a301424b068ebc6387966985197
KwonSeongmin <kwonsm@icloud.com>
2017-06-02 00:09:02 +0900

    uri에서 path만 가져올 수 있도록 수정(QueryStringDecoder사용)

--- exof/src/main/java/team/balam/exof/module/listener/handler/transform/HttpTransform.java
+++ exof/src/main/java/team/balam/exof/module/listener/handler/transform/HttpTransform.java
@@ -1,6 +1,7 @@
 package team.balam.exof.module.listener.handler.transform;
 
 import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.QueryStringDecoder;
 import team.balam.exof.module.service.ServiceObject;
 
 public class HttpTransform implements ServiceObjectTransform<FullHttpRequest>
@@ -9,7 +10,8 @@ public class HttpTransform implements ServiceObjectTransform<FullHttpRequest>
 	@Override
 	public ServiceObject transform(FullHttpRequest _msg) throws Exception 
 	{
-		ServiceObject serviceObject = new ServiceObject(_msg.uri());
+		QueryStringDecoder decoder = new QueryStringDecoder(_msg.uri());
+		ServiceObject serviceObject = new ServiceObject(decoder.path());
 		serviceObject.setRequest(_msg);
 
 		return serviceObject;
