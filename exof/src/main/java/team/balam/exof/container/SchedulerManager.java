0ceb77e007782434c4e0dcfe07913f34d7b9384e
KwonSeongmin <kwonsm@icloud.com>
2017-06-24 20:36:35 +0900

    스케쥴러 파일 변경 감지 업데이트 시 새로운 스케쥴러 null 체크

--- exof/src/main/java/team/balam/exof/container/SchedulerManager.java
+++ exof/src/main/java/team/balam/exof/container/SchedulerManager.java
@@ -34,8 +34,7 @@ public class SchedulerManager implements Container, Observer
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	private Scheduler scheduler;
 	private boolean isAutoReload;
-	private long updateVariableTime;
-	
+
 	private Map<String, JobKey> jobKeyMap = new HashMap<>();
 	
 	private static SchedulerManager self = new SchedulerManager();
@@ -138,34 +137,30 @@ public class SchedulerManager implements Container, Observer
 		
 		if(! this.isAutoReload) return;
 		
-		if(System.currentTimeMillis() - this.updateVariableTime > 5000)
-		{
-			this.updateVariableTime = System.currentTimeMillis();
-			
-			infoList.forEach(_info -> {
-				JobKey jobkey = this.jobKeyMap.get(_info.getId());
-				
+		infoList.forEach(_info -> {
+			JobKey jobkey = this.jobKeyMap.get(_info.getId());
+			if (jobkey != null) {
 				try
 				{
 					JobDataMap dataMap = this.scheduler.getJobDetail(jobkey).getJobDataMap();
 					SchedulerInfo realInfo = (SchedulerInfo)dataMap.get("info");
-					
+
 					if(_info.isUse() != realInfo.isUse())
 					{
 						realInfo.setUse(_info.isUse());
-						
+
 						if(! _info.isUse()) this.scheduler.pauseJob(jobkey);
 						else this.scheduler.resumeJob(jobkey);
-						
+
 						this.logger.warn("Complete reloading schedulerInfo. [{}]", _info.getServicePath());
 					}
-				} 
+				}
 				catch(Exception e)
 				{
 					this.logger.error("Can not update scheduler. [{}]", _info.toString(), e);
 				}
-			});
-		}
+			}
+		});
 	}
 	
 	public List<String> getScheduleList()
