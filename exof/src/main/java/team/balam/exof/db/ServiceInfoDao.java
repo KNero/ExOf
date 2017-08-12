9c58cfe27a1ca392babe2de967bc848adb58be2d
KwonSeongmin <kwonsm@icloud.com>
2017-08-12 19:31:56 +0900

    에러 처리 변경 및 service.xml 관련 select 추

--- exof/src/main/java/team/balam/exof/db/ServiceInfoDao.java
+++ exof/src/main/java/team/balam/exof/db/ServiceInfoDao.java
@@ -1,14 +1,19 @@
 package team.balam.exof.db;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import team.balam.exof.Constant;
-import team.balam.exof.container.scheduler.SchedulerInfo;
 import team.balam.exof.environment.LoadEnvException;
+import team.balam.exof.environment.vo.SchedulerInfo;
+import team.balam.exof.environment.vo.ServiceDirectoryInfo;
+import team.balam.exof.environment.vo.ServiceVariableInfo;
 import team.balam.util.sqlite.connection.PoolManager;
 import team.balam.util.sqlite.connection.vo.QueryTimeoutException;
 import team.balam.util.sqlite.connection.vo.QueryVo;
 import team.balam.util.sqlite.connection.vo.QueryVoFactory;
-import team.balam.util.sqlite.connection.vo.Result;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -17,6 +22,8 @@ import java.util.Map;
  * Service 를 reload 하기 위해서 저장하며 서버가 재기동 될 때 마다 초기화 된다.
  */
 public class ServiceInfoDao {
+	private static final Logger LOGGER = LoggerFactory.getLogger(ServiceInfoDao.class);
+
 	public static void createTable() throws Exception {
 		initServiceDirectoryTable();
 		initServiceVariableTable();
@@ -51,8 +58,7 @@ public class ServiceInfoDao {
 				"SERVICE_DIRECTORY_PATH TEXT NOT NULL, " +
 				"SERVICE TEXT, " +
 				"KEY TEXT, " +
-				"VALUE TEXT," +
-				"PRIMARY KEY(SERVICE_DIRECTORY_PATH, SERVICE))";
+				"VALUE TEXT)";
 		QueryVo vo = QueryVoFactory.create(QueryVo.Type.EXECUTE);
 		vo.setQuery(query);
 
@@ -62,7 +68,7 @@ public class ServiceInfoDao {
 		}
 
 		vo = QueryVoFactory.create(QueryVo.Type.DELETE);
-		vo.setQuery("DELETE FROM SERVICE_DIRECTORY");
+		vo.setQuery("DELETE FROM SERVICE_VARIABLE");
 
 		PoolManager.getInstance().executeQuery(Constant.ENV_DB, vo);
 		if (!vo.getResult().isSuccess()) {
@@ -129,7 +135,7 @@ public class ServiceInfoDao {
 
 		try {
 			if (!vo.getResult().isSuccess()) {
-				throw new LoadEnvException("fail insert to SERVICE_DIRECTORY table.");
+				throw new LoadEnvException("fail insert to SERVICE_DIRECTORY table.", vo.getResult().getException());
 			}
 		} catch (QueryTimeoutException e) {
 			throw new LoadEnvException("query time out.", e);
@@ -146,7 +152,7 @@ public class ServiceInfoDao {
 
 		try {
 			if (!vo.getResult().isSuccess()) {
-				throw new LoadEnvException("fail insert to SERVICE_VARIABLE table.");
+				throw new LoadEnvException("fail insert to SERVICE_VARIABLE table.", vo.getResult().getException());
 			}
 		} catch (QueryTimeoutException e) {
 			throw new LoadEnvException("query time out.", e);
@@ -155,7 +161,7 @@ public class ServiceInfoDao {
 
 	public static void insertSchedule(String _id, String _servicePath, String _cron, String _duplicateExecution, String _use, String _initExecution)
 			throws LoadEnvException {
-		String query = "INSERT INTO SCHEDULE(ID, SERVICE_PATH, CRON, DUPLICATE_EXECUTION, USE, INIT_EXECUTION) VALUES (?, ?, ?, ?, ?)";
+		String query = "INSERT INTO SCHEDULE(ID, SERVICE_PATH, CRON, DUPLICATE_EXECUTION, USE, INIT_EXECUTION) VALUES (?, ?, ?, ?, ?, ?)";
 		QueryVo vo = QueryVoFactory.create(QueryVo.Type.INSERT);
 		vo.setQuery(query);
 		vo.setParam(new Object[]{_id, _servicePath, _cron, _duplicateExecution, _use, _initExecution});
@@ -164,36 +170,86 @@ public class ServiceInfoDao {
 
 		try {
 			if (!vo.getResult().isSuccess()) {
-				throw new LoadEnvException("fail insert to SCHEDULE table.");
+				throw new LoadEnvException("fail insert to SCHEDULE table.", vo.getResult().getException());
 			}
 		} catch (QueryTimeoutException e) {
 			throw new LoadEnvException("query time out.", e);
 		}
 	}
 
-	public static SchedulerInfo selectSchedule(String _id) throws Exception {
-		String query = "SELECT * FROM SCHEDULE WHERE ID=?";
-		QueryVo vo = QueryVoFactory.create(QueryVo.Type.SELECT);
-		vo.setQuery(query);
-		vo.setParam(new Object[]{_id});
-
-		PoolManager.getInstance().executeQuery(Constant.ENV_DB, vo);
-
-		Result result = null;
+	public static List<ServiceDirectoryInfo> selectServiceDirectory() {
+		String query = "SELECT * FROM SERVICE_DIRECTORY";
 
 		try {
-			result = vo.getResult();
-			List<Map<String, Object>> resultList = result.getSelectResult();
+			List<Map<String, Object>> selectList = DbHelper.select(query, null);
+
+			if (!selectList.isEmpty()) {
+				List<ServiceDirectoryInfo> resultList = new ArrayList<>();
+				for (Map<String, Object> row : selectList) {
+					resultList.add(new ServiceDirectoryInfo(row));
+				}
+
+				return resultList;
+			}
+		} catch (Exception e) {
+			LOGGER.error("Error occurred execute query.", e);
+		}
+
+		return Collections.emptyList();
+	}
+
+	public static ServiceVariableInfo selectServiceVariable(String _serviceDirectoryPath, String _serviceName) {
+		String query = "SELECT KEY, VALUE FROM SERVICE_VARIABLE WHERE SERVICE_DIRECTORY_PATH=? AND SERVICE=?";
+		Object[] param = new Object[]{_serviceDirectoryPath, _serviceName};
+
+		try {
+			List<Map<String, Object>> selectList = DbHelper.select(query, param);
+
+			if (!selectList.isEmpty()) {
+				return new ServiceVariableInfo(selectList);
+			}
+		} catch (Exception e) {
+			LOGGER.error("Error occurred execute query.", e);
+		}
+
+		return ServiceVariableInfo.NULL_OBJECT;
+	}
+
+	public static SchedulerInfo selectSchedule(String _id) {
+		String query = "SELECT * FROM SCHEDULE WHERE ID=?";
+		Object[] param = new Object[]{_id};
+
+		try {
+			List<Map<String, Object>> resultList = DbHelper.select(query, param);
 
 			if (!resultList.isEmpty()) {
 				return new SchedulerInfo(resultList.get(0));
-			} else {
-				return SchedulerInfo.NULL_OBJECT;
 			}
-		} finally {
-			if (result != null) {
-				result.close();
-			}
+		} catch (Exception e) {
+			LOGGER.error("Error occurred execute query.", e);
 		}
+
+		return SchedulerInfo.NULL_OBJECT;
+	}
+
+	public static List<SchedulerInfo> selectSchedule() {
+		String query = "SELECT * FROM SCHEDULE";
+
+		try {
+			List<Map<String, Object>> selectList = DbHelper.select(query, null);
+
+			if (!selectList.isEmpty()) {
+				List<SchedulerInfo> resultList = new ArrayList<>();
+				for (Map<String, Object> row : selectList) {
+					resultList.add(new SchedulerInfo(row));
+				}
+
+				return resultList;
+			}
+		} catch (Exception e) {
+			LOGGER.error("Error occurred execute query.", e);
+		}
+
+		return Collections.emptyList();
 	}
 }
