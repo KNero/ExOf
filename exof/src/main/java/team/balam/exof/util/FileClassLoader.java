96422707640e35da7899813236b3195abc5393e5
smkwon <smkwon@miridih.com>
2017-09-29 12:35:10 +0900

    FileClassLoader refactoring

--- exof/src/main/java/team/balam/exof/util/FileClassLoader.java
+++ exof/src/main/java/team/balam/exof/util/FileClassLoader.java
@@ -2,54 +2,42 @@ package team.balam.exof.util;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 
-public class FileClassLoader 
-{
-	public static void loadJar( String _path ) throws Exception
-	{
-		File f = new File( _path );
-		
-		if( f.isDirectory() )
-		{
-			for( String fileName : f.list() )
-			{
-				FileClassLoader.loadJar( _path + "/" + fileName );
+public class FileClassLoader extends URLClassLoader {
+
+	public FileClassLoader(ClassLoader _parent, URL... _urls) {
+		super(_urls, _parent);
+	}
+
+	public FileClassLoader(URL... _urls) {
+		super(_urls, ClassLoader.getSystemClassLoader());
+	}
+
+
+	/**
+	 * jar 파일을 로딩하며 디렉토리가 들어올 경우 반복적으로 내부 파일을 찾아서 로딩한다.
+	 * @param _file jar file or directory
+	 * @throws IOException url 을 가져올 수 없을 경우 발생한다.
+	 */
+	public void loadJar(File _file) throws IOException {
+		if (_file != null && _file.exists()) {
+			for (String fileList : _file.list()) {
+				File childFile = new File(_file, fileList);
+
+				if (_file.isDirectory()) {
+					this.loadJar(childFile);
+				} else if (_file.getName().endsWith(".jar")) {
+					this.addURL(_file.toURI().toURL());
+				}
 			}
 		}
-		else if( f.isFile() && f.getName().endsWith( ".jar" ) )
-		{
-			FileClassLoader.loadFileOrDirectory( f );
-		}
 	}
-	
-	public static void loadFileOrDirectory( String _s ) throws Exception
-	{
-		File f = new File( _s );
-		FileClassLoader.loadFileOrDirectory( f );
-	}
-	
-	public static void loadFileOrDirectory( File _f ) throws IOException
-	{
-		FileClassLoader.loadUrl( _f.toURI( ).toURL( ) );
-	}
-	
-	public static void loadUrl( URL _u ) throws IOException
-	{
-		URLClassLoader sysloader = ( URLClassLoader ) ClassLoader.getSystemClassLoader( );
-		Class< URLClassLoader > sysclass = URLClassLoader.class;
-		
-		try
-		{
-			Method method = sysclass.getDeclaredMethod("addURL" , new Class[]{URL.class});
-			method.setAccessible( true );
-			method.invoke( sysloader , new Object[] { _u } );
-		}
-		catch( Exception e )
-		{
-			throw new IOException( "Error, could not add URL to system classloader" );
-		}
+
+	public static void main(String[] a) throws Exception {
+		FileClassLoader loader =
+				new FileClassLoader(new File("./lib/SQLite-Helper-0.2.0-SNAPSHOT.jar").toURI().toURL());
+		loader.close();
 	}
 }
