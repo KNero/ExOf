6bcb3c93e434918a0ea81b776e7599660b334450
KwonSeongmin <kwonsm@icloud.com>
2017-10-07 23:05:36 +0900

    자동 배포 기능 테스트를 위한 jar 추가 및 리팩토링

--- exof/src/main/java/team/balam/exof/ExofWebListener.java
+++ exof/src/main/java/team/balam/exof/ExofWebListener.java
@@ -4,6 +4,7 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 import team.balam.exof.environment.EnvKey;
+import team.balam.exof.environment.SystemSetting;
 
 import java.io.File;
 
@@ -12,10 +13,10 @@ public class ExofWebListener implements ServletContextListener {
 	public void contextInitialized(ServletContextEvent arg0) {
 		String configPath = arg0.getServletContext().getInitParameter("exof.home");
 		if(configPath != null && configPath.trim().length() > 0) {
-			System.setProperty(EnvKey.HOME, configPath.trim());
+			SystemSetting.setFramework(EnvKey.HOME, configPath.trim());
 		} else {
 			String currentPath = new File(".").getAbsolutePath();
-			System.setProperty(EnvKey.HOME, currentPath);
+			SystemSetting.setFramework(EnvKey.HOME, currentPath);
 		}
 		
 		App.start();
--- exof/src/main/java/team/balam/exof/module/service/component/InboundExecuteException.java
+++ exof/src/main/java/team/balam/exof/module/service/component/InboundExecuteException.java
@@ -1,16 +1,13 @@
 package team.balam.exof.module.service.component;
 
-public class InboundExecuteException extends Exception 
-{
+public class InboundExecuteException extends Exception {
 	private static final long serialVersionUID = -8935499671612950840L;
 	
-	public InboundExecuteException(Exception _e)
-	{
+	public InboundExecuteException(String _msg, Exception _e) {
 		super(_e);
 	}
 	
-	public InboundExecuteException(String _msg, Exception _e)
-	{
-		super(_msg, _e);
+	public InboundExecuteException(String _msg) {
+		super(_msg);
 	}
 }
