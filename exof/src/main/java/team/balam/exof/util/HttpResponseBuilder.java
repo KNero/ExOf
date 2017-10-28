ce14be8c8fdc3caf567cf5534356158b9982914b
KwonSeongmin <kwonsm@icloud.com>
2017-10-28 17:26:02 +0900

    응답 메시지를  기본으로 json 으로 전송하는 기능 제거

--- exof/src/main/java/team/balam/exof/util/HttpResponseBuilder.java
+++ exof/src/main/java/team/balam/exof/util/HttpResponseBuilder.java
@@ -28,24 +28,19 @@ public class HttpResponseBuilder
 	}
 
 	public static FullHttpResponse buildOkMessage(String _msg) {
-		Map<String, Object> content = new HashMap<>();
-		content.put("Message", _msg);
+		return createHttpResponse(HttpResponseStatus.OK, _msg);
+	}
 
-		return createHttpResponse(HttpResponseStatus.OK, content);
+	public static FullHttpResponse buildOkJson(Map<String, Object> data) {
+		return createHttpResponse(HttpResponseStatus.OK, data);
 	}
 
 	public static FullHttpResponse buildServerError(String _msg) {
-		Map<String, Object> content = new HashMap<>();
-		content.put("Error", _msg);
-
-		return createHttpResponse(HttpResponseStatus.INTERNAL_SERVER_ERROR, content);
+		return createHttpResponse(HttpResponseStatus.INTERNAL_SERVER_ERROR, _msg);
 	}
 
 	public static FullHttpResponse buildBadRequest(String _msg) {
-		Map<String, Object> content = new HashMap<>();
-		content.put("Error", _msg);
-
-		return createHttpResponse(HttpResponseStatus.BAD_REQUEST, content);
+		return createHttpResponse(HttpResponseStatus.BAD_REQUEST, _msg);
 	}
 
 	public static FullHttpResponse buildNotImplemented(String _msg) {
