887c17e419960b1a8a1b2a82161ee269d029a02b
SeongminKwon <kwon-s-m@hanmail.net>
2019-04-15 15:55:00 +0900

    loading mybatis 개선

--- exof/src/main/java/team/balam/exof/db/DbSessionFactory.java
+++ exof/src/main/java/team/balam/exof/db/DbSessionFactory.java
@@ -4,96 +4,63 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import team.balam.exof.environment.EnvKey;
-import team.balam.exof.environment.LoadEnvException;
-import team.balam.exof.environment.MyBatisLoader;
-import team.balam.exof.environment.SystemSetting;
 
-import java.io.File;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class DbSessionFactory 
-{
-	private Logger logger = LoggerFactory.getLogger(this.getClass());
-	
-	private SqlSessionFactory defaultSqlSessionFactory;
-	private Map<String, SqlSessionFactory> factoryMap = new ConcurrentHashMap<>();
-	
-	private static DbSessionFactory self = new DbSessionFactory();
-	
-	private DbSessionFactory()
-	{
-		
-	}
-	
-	public static DbSessionFactory getInstance()
-	{
-		return self;
-	}
-	
-	public SqlSessionFactory getDefaultSqlSessionFactory()
-	{
-		return this.defaultSqlSessionFactory;
-	}
-	
-	public SqlSessionFactory getSqlSessionFactory(String _datasource) throws DatasourceNotLoadException
-	{
-		SqlSessionFactory factory = this.factoryMap.get(_datasource);
-		
-		if(factory == null)
-		{
-			throw new DatasourceNotLoadException(_datasource);
-		}
-		
-		return factory;
-	}
-	
-	public SqlSession getDefaultSqlSession()
-	{
-		return this.defaultSqlSessionFactory.openSession(false);
-	}
-	
-	public void setDefaultSqlSessionFactory(SqlSessionFactory _factory)
-	{
-		if(this.defaultSqlSessionFactory == null)
-		{
-			this.defaultSqlSessionFactory = _factory;
-		}
-		else
-		{
-			if(this.logger.isWarnEnabled())
-			{
-				this.logger.warn("Default sqlSessionFactory was already registered.");
-			}
-		}
-	}
-	
-	public SqlSession getSqlSession(String _datasource) throws DatasourceNotLoadException
-	{
-		return this.getSqlSessionFactory(_datasource).openSession();
-	}
-	
-	public SqlSession getSqlSession(String _datasource, boolean _isAutoCommit) throws DatasourceNotLoadException
-	{
-		return this.getSqlSessionFactory(_datasource).openSession(_isAutoCommit);
-	}
-	
-	public void loadSqlSessionFactory(String _datasource) {
-		MyBatisLoader loader = new MyBatisLoader();
-		String home = SystemSetting.getFramework(EnvKey.HOME);
+public class DbSessionFactory {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-		File config = new File(home + "/env/mybatis-config.xml");
-		if (!config.exists() || config.length() == 0) {
-			logger.error("resource is not exists. {}", config.getAbsolutePath());
-			return;
-		}
-		
-		try {
-			SqlSessionFactory factory = loader.loadSqlSessionFactory(config.getAbsoluteFile(), _datasource);
-			this.factoryMap.put(_datasource, factory);
-		} catch (LoadEnvException e) {
-			this.logger.error("Can not load sqlSessionFactory", e);
-		}
-	}
+    private SqlSessionFactory defaultSqlSessionFactory;
+    private Map<String, SqlSessionFactory> factoryMap = new ConcurrentHashMap<>();
+
+    private static DbSessionFactory self = new DbSessionFactory();
+
+    private DbSessionFactory() {
+
+    }
+
+    public static DbSessionFactory getInstance() {
+        return self;
+    }
+
+    public void putSqlSessionFactory(String datasource, SqlSessionFactory factory) {
+        factoryMap.put(datasource, factory);
+    }
+
+    public SqlSessionFactory getSqlSessionFactory(String _datasource) throws DatasourceNotLoadException {
+        SqlSessionFactory factory = this.factoryMap.get(_datasource);
+
+        if (factory == null) {
+            throw new DatasourceNotLoadException(_datasource);
+        }
+
+        return factory;
+    }
+
+    public SqlSessionFactory getDefaultSqlSessionFactory() {
+        return this.defaultSqlSessionFactory;
+    }
+
+    public SqlSession getDefaultSqlSession() {
+        return this.defaultSqlSessionFactory.openSession(false);
+    }
+
+    public void setDefaultSqlSessionFactory(SqlSessionFactory _factory) {
+        if (this.defaultSqlSessionFactory == null) {
+            this.defaultSqlSessionFactory = _factory;
+        } else {
+            if (this.logger.isWarnEnabled()) {
+                this.logger.warn("Default sqlSessionFactory was already registered.");
+            }
+        }
+    }
+
+    public SqlSession getSqlSession(String _datasource) throws DatasourceNotLoadException {
+        return this.getSqlSessionFactory(_datasource).openSession();
+    }
+
+    public SqlSession getSqlSession(String _datasource, boolean _isAutoCommit) throws DatasourceNotLoadException {
+        return this.getSqlSessionFactory(_datasource).openSession(_isAutoCommit);
+    }
 }
--- exof/src/main/java/team/balam/exof/environment/MyBatisLoader.java
+++ exof/src/main/java/team/balam/exof/environment/MyBatisLoader.java
@@ -1,18 +1,24 @@
 package team.balam.exof.environment;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-
+import io.netty.util.internal.StringUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 import team.balam.exof.db.DbSessionFactory;
 
-public class MyBatisLoader implements Loader {
-    private Logger log = LoggerFactory.getLogger(this.getClass());
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
+import java.io.*;
 
+@Slf4j
+public class MyBatisLoader implements Loader {
 	@Override
 	public void load(String _envPath) throws LoadEnvException {
 		File mybatis = new File(_envPath + "/mybatis-config.xml");
@@ -21,19 +27,54 @@ public class MyBatisLoader implements Loader {
 		    return;
         }
 
-		org.apache.ibatis.session.SqlSessionFactory defaultSqlSessionFactory = this.loadSqlSessionFactory(mybatis, null);
-		DbSessionFactory.getInstance().setDefaultSqlSessionFactory(defaultSqlSessionFactory);
-	}
-	
-	public org.apache.ibatis.session.SqlSessionFactory loadSqlSessionFactory(File mybatisConfig, String _datasource) throws LoadEnvException {
-		try (InputStream inputStream = new FileInputStream(mybatisConfig)) {
-			if(_datasource != null) {
-				return new SqlSessionFactoryBuilder().build(inputStream, _datasource);
-			} else {
-				return new SqlSessionFactoryBuilder().build(inputStream);
+		try (BufferedReader reader = new BufferedReader(new FileReader(mybatis))) {
+			InputSource inputSource = new InputSource(reader);
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			Document document = factory.newDocumentBuilder().parse(inputSource);
+
+			XPath xPath = XPathFactory.newInstance().newXPath();
+
+			XPathExpression expression = xPath.compile("//environments");
+			Node node = (Node) expression.evaluate(document, XPathConstants.NODE);
+
+			loadSqlSessionFactory(mybatis, node, "default", true);
+
+			expression = xPath.compile("//environments/environment");
+			NodeList nodeList = (NodeList) expression.evaluate(document, XPathConstants.NODESET);
+			for (int i = 0; i < nodeList.getLength(); i++) {
+				node = nodeList.item(i);
+
+				loadSqlSessionFactory(mybatis, node, "id", false);
 			}
-		} catch(Exception e) {
-			throw new LoadEnvException("mybatis-config.xml", e);
+		} catch (Exception e) {
+			throw new LoadEnvException("Fail to load mybatis config file.", e);
 		}
+	}
+
+	private static void loadSqlSessionFactory(File configFile, Node node, String attrName, boolean isDefault) throws IOException {
+		if (node != null && node.getAttributes() != null) {
+			NamedNodeMap attr = node.getAttributes();
+			if (attr != null) {
+				Node attrNode = attr.getNamedItem(attrName);
+				if (attrNode != null && !StringUtil.isNullOrEmpty(attrNode.getNodeValue())) {
+
+					try (InputStream inputStream = new FileInputStream(configFile)) {
+						org.apache.ibatis.session.SqlSessionFactory factory =
+								new SqlSessionFactoryBuilder().build(inputStream, attrNode.getNodeValue());
+
+						if (isDefault) {
+							DbSessionFactory.getInstance().setDefaultSqlSessionFactory(factory);
+						} else {
+							DbSessionFactory.getInstance().putSqlSessionFactory(attrNode.getNodeValue(), factory);
+						}
+
+						log.info("loaded datasource: {}", attrNode.getNodeValue());
+						return;
+					}
+				}
+			}
+		}
+
+		log.warn("mybatis-config.xml environments default attribute is null.");
 	}
 }
